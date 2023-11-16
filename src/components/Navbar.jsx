dc6ad2024113af57160eca9c156762338837814f
Yehui Huang <yehui.usyd@gmail.com>
2023-11-16 19:16:19 +1100

    v1.3 Adjust margin for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto text-gray-600'>
-            <h1 className='w-full text-3xl font-bold text-[#6495ED]'>Contacts.</h1>
-            <ul className='flex'>
-                <a href={'/'} className='p-4'>Home</a>
-                <a href={'/'} className='p-4'>About</a>
-            </ul>
+        <div className='ml-10 mr-10'>
+            <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto text-gray-600'>
+                <h1 className='w-full text-3xl font-bold text-[#6495ED]'>Contacts.</h1>
+                <ul className='flex'>
+                    <a href={'/'} className='p-4'>Home</a>
+                    <a href={'/'} className='p-4'>About</a>
+                </ul>
+            </div>
         </div>
+
     );
 }
 
